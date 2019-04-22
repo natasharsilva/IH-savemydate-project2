09d1f7fe035062f4974ab3ba3acd0488082369d7
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-04-22 14:37:54 +0100

    testing API

--- models/Date.js
+++ models/Date.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const dateSchema = new Schema({
+  entity_id: {type: Number, default: 82},
+  entity_type: {type: String, default: "city"},
   placeId: String,
-  
+
 }, {
   timestamps: {
     createdAt: 'created_at',
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+const zomatoApi = axios.create({baseUrl: "https://developers.zomato.com/api/v2.1/search?"})
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
 
 module.exports = router;
 
@@ -12,5 +14,12 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 router.post('/searchPlace', (req, res, next) => {
+  const theId = req.body.value;
+  axios.post("https://developers.zomato.com/api/v2.1/search?", theId )
+    .then(response => {
+       Date.create({
+        placeId: theId
+       })
+    })
   res.render('place-details');
 });
