613f9f123c3bc07dc5d818df3b90ec0a7720d963
Carla Mendes <carlarsmendes@gmail.com>
2019-04-22 13:12:21 +0100

    added cinema model for DB creation

--- bin/seeds.js
+++ bin/seeds.js
@@ -6,6 +6,8 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const Date = require("../models/Date");
+const Cinema = require("../models/Cinema");
 
 const bcryptSalt = 10;
 
@@ -36,6 +38,46 @@ User.deleteMany()
 .then(usersCreated => {
   console.log(`${usersCreated.length} users created with the following id:`);
   console.log(usersCreated.map(u => u._id));
+})
+.then(() => {
+  // Close properly the connection to Mongoose
+  mongoose.disconnect()
+})
+.catch(err => {
+  mongoose.disconnect()
+  throw err
+})
+
+
+let cinemas = [
+  {  name: String,
+    "location": {
+      "address": "D.N.R College Road, Bhimavaram Locality, Bhimavaram",
+      "locality": "Bhimavaram Locality",
+      "city": "Bhimavaram",
+      "city_id": 82,
+      "latitude": "16.5437590000",
+      "longitude": "81.5165070000",
+      "zipcode": "",
+      "country_id": 164,
+      "country_name": "Portugal",
+    },
+  }, {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+]
+
+
+Cinema.deleteMany()
+.then(() => {
+  return Cinema.create(cinemas)
+})
+.then(cinemasCreated => {
+  console.log(`${cinemasCreated.length} cinemas created with the following id:`);
+  console.log(cinemasCreated.map(u => u._id));
 })
 .then(() => {
   // Close properly the connection to Mongoose
