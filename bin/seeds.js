573f4a44a29a4486c58eb43b2bf6a647f1736116
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-04-22 20:12:44 +0100

    login with email

--- bin/seeds.js
+++ bin/seeds.js
@@ -22,11 +22,11 @@ mongoose
 
 let users = [
   {
-    username: "alice",
+    email: "alice",
     password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
   },
   {
-    username: "bob",
+    email: "bob",
     password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
   }
 ]
