274d01fbd500edf4ba32ac040e5a1de16f8894a7
Guilherme Carmona <guilherme.carmona@outlook.com>
2019-04-23 09:52:56 +0100

    adjust user model email requirements

--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: String,
+  email: { 
+    type: String,
+    required: true,
+    unique: true,
+    match: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  },
   password: String
 }, {
   timestamps: {
