363aac74f8d96c2b186356d047002764e87e1cd3
Maxence Bouret <maxence.bouret@gmail.com>
2019-04-25 13:37:47 +0100

    Review with Maxence

---
+++ bin/data/cinemas.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    name: "Cinemas NOS Amoreiras",
+    address: "Av. Eng. Duarte Pacheco n° 2037, 1070-103 Lisboa",
+    latitude: 38.723759,
+    longitude: -9.161578,
+    website: "http://cinemas.nos.pt/Cinemas/Pages/amoreiras.aspx",
+  },
+  {
+    name: "UCI Cinema El Corte Inglés",
+    address: "Av. António Augusto de Aguiar 31, 1069-413 Lisboa",
+    latitude: 38.732609,
+    longitude: -9.153369,
+    website: "http://www.ucicinemas.pt/Cinemas/El-Corte-Ingles",
+  },
+  {
+    name: "Cinemateca Portuguesa - Museu do Cinema",
+    address: "R. Barata Salgueiro 39, 1250-165 Lisboa",
+    latitude: 38.720881,
+    longitude: -9.148614,
+    website: "http://www.cinemateca.pt/",
+  },
+  {
+    name: "Cinema Ideal",
+    address: "Rua do Loreto 15, 1200-086 Lisboa",
+    latitude: 38.710655,
+    longitude: -9.144251,
+    website: "http://www.cinemaideal.pt/",
+  },
+  {
+    name: "IndieLisboa",
+    address: "Rua da Rosa, 277, 2.º - Sala 1.4, 1200-385 Lisboa",
+    latitude: 38.715204,
+    longitude: -9.145953,
+    website: "https://indielisboa.com/",
+  },
+  {
+    name: "Cinemas NOS Dolce Vita Miraflores",
+    address: "Av. das Túlipas, 1495-611 Dolce Vita Miraflores",
+    latitude: 38.709240,
+    longitude: -9.227393,
+    website: "http://cinemas.nos.pt/cinemas/Pages/dolce-vita-miraflores.aspx",
+  },
+]
